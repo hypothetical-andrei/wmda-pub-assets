7b887f41ddcc57666c715495978b2b7183804bb3
AT <hypothetical.andrei@gmail.com>
2022-04-05 18:24:45 +0300

    .

--- c7/tf-w2v.py
+++ c7/tf-w2v.py
@@ -1,3 +1,5 @@
+import tensorflow as tf
+
 sentence = "this is a sentence and it is a good sentence"
 
 tokens = list(sentence.lower().split())
@@ -18,7 +20,22 @@ for token in tokens:
 inverse_vocab = {index : token for token, index in vocab.items()}
 
 # print(inverse_vocab)
+vocabulary_size = len(vocab)
 
 example_sequence = [vocab[word] for word in tokens]
 
 print(example_sequence)
+
+window_size = 2
+positive_skip_grams, _ = tf.keras.preprocessing.sequence.skipgrams(
+  example_sequence,
+  vocabulary_size = vocabulary_size,
+  window_size = window_size,
+  negative_samples = 0
+)
+
+print(len(positive_skip_grams))
+print(positive_skip_grams[:5])
+
+for target, context in positive_skip_grams[:5]:
+  print(f"({target}, {context}): ({inverse_vocab[target]}, {inverse_vocab[context]})")
